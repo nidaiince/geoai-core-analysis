9c9b5fc0d9ebd10edddd9e18c1098ab07794de02
nidaiince <nidaiincee@icloud.com>
2026-05-18 11:04:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,56 +27,56 @@ if uploaded_file:
     # Tahmin
     results = model(temp_path)
 
-  names = model.names
+    names = model.names
 
-boxes = []
-classes = []
+    boxes = []
+    classes = []
 
-if results[0].boxes is not None:
-    boxes = results[0].boxes.xyxy.cpu().numpy()
-    classes = results[0].boxes.cls.cpu().numpy()
+    if results[0].boxes is not None:
+        boxes = results[0].boxes.xyxy.cpu().numpy()
+        classes = results[0].boxes.cls.cpu().numpy()
 
-core_count = 0
+    core_count = 0
 
-total_core = 0
-rqd_core = 0
-scr_core = 0
+    total_core = 0
+    rqd_core = 0
+    scr_core = 0
 
-for box, cls in zip(boxes, classes):
+    for box, cls in zip(boxes, classes):
 
-    label = names[int(cls)]
+        label = names[int(cls)]
 
-    if label == "core_segment":
+        if label == "core_segment":
 
-        core_count += 1
+            core_count += 1
 
-        x1, y1, x2, y2 = box
+            x1, y1, x2, y2 = box
 
-        core_length = (x2 - x1) / image.width
+            core_length = (x2 - x1) / image.width
 
-        total_core += core_length
+            total_core += core_length
 
-        if core_length >= 0.10:
-            rqd_core += core_length
+            if core_length >= 0.10:
+                rqd_core += core_length
 
-        if core_length >= 0.30:
-            scr_core += core_length
+            if core_length >= 0.30:
+                scr_core += core_length
 
-rqd = rqd_core * 100
-tcr = total_core * 100
-scr = scr_core * 100
+    rqd = rqd_core * 100
+    tcr = total_core * 100
+    scr = scr_core * 100
 
-if rqd > 100:
-    rqd = 100
+    if rqd > 100:
+        rqd = 100
 
-if tcr > 100:
-    tcr = 100
+    if tcr > 100:
+        tcr = 100
 
-if scr > 100:
-    scr = 100
+    if scr > 100:
+        scr = 100
 
-st.success(f"Detected Core Segments: {core_count}")
+    st.success(f"Detected Core Segments: {core_count}")
 
-st.metric("RQD", f"{rqd:.2f}%")
-st.metric("TCR", f"{tcr:.2f}%")
-st.metric("SCR", f"{scr:.2f}%")
+    st.metric("RQD", f"{rqd:.2f}%")
+    st.metric("TCR", f"{tcr:.2f}%")
+    st.metric("SCR", f"{scr:.2f}%")
